ea0641d16ea792c9a562b55dbc74e0397b6ed202
Ricardo Kagawa <ricardo.kagawa@metasix.com.br>
2014-12-29 16:14:01 -0200

    Binding and listening on server socket, and started to accept connections

--- server.c
+++ server.c
@@ -12,7 +12,59 @@
 #include <sys/socket.h>
 #include <unistd.h>
 
+// the default ulimit -Sn is usually 1024, but
+// take the default file descriptors into account
+#define BACKLOG_SIZE    1000
 #define DEFAULT_SERVICE "8080"
+
+#define E_NONE      0
+#define E_ADDRINFO  1
+#define E_SSOCKET   2
+#define E_SS_BIND   3
+#define E_SS_LISTEN 4
+
+/**
+ * Outputs a corresponding error message to stderr.
+ */
+void error(int cat, int code) {
+    switch(cat) {
+        case E_ADDRINFO:
+            fputs(gai_strerror(code), stderr);
+            break;
+
+        case E_SSOCKET:
+            fputs("Failed to open server socket", stderr);
+            switch(code) {
+                case EACCES:
+                    fputs(": access denied", stderr);
+                    break;
+                case ENOBUFS:
+                case ENOMEM:
+                    fputs(": out of memory", stderr);
+                    break;
+            }
+            break;
+
+        case E_SS_BIND:
+            fputs("Failed to bind server socket", stderr);
+            switch(code) {
+                case EACCES:
+                    fputs(": access denied", stderr);
+                    break;
+                case EADDRINUSE:
+                    fputs(": address already in use", stderr);
+                    break;
+            }
+            break;
+
+        case E_SS_LISTEN:
+            fputs("Failed to listen on server socket", stderr);
+            if (code == EADDRINUSE)
+                fputs(": address already in use", stderr);
+            break;
+    }
+    fputc('\n', stderr);
+}
 
 /**
  * Calls getaddrinfo(3) with hints suitable for an HTTP server. _service_
@@ -34,43 +86,18 @@ struct addrinfo* get_server_addrinfo(char service[]) {
     h.ai_next = NULL;
 
     rv = getaddrinfo(NULL, service, &h, &r);
-
     if (rv != 0) {
-        fputs(gai_strerror(EAI_AGAIN), stderr);
-        fputc('\n', stderr);
+        error(E_ADDRINFO, rv);
         return NULL;
     } else
         return r;
 }
 
 /**
- * Creates an unbound server socket using the given address info. If the
- * socket could not be created, an error message is printed to stderr and
- * -1 is returned.
+ * Opens a listening server socket to accept TCP connections. The socket's
+ * file descriptor is returned on success. _service_ can be a service name
+ * (see services(5)) or a decimal port number.
  */
-int create_server_socket(struct addrinfo *ai) {
-    int fd, ecode;
-
-    fd = socket(ai->ai_family, ai->ai_socktype, ai->ai_protocol);
-    if (fd < 0) {
-        ecode = errno;
-        fputs("Could not open server socket", stderr);
-        switch(ecode) {
-            case EACCES:
-                fputs(": access denied\n", stderr);
-                break;
-            case ENOBUFS:
-            case ENOMEM:
-                fputs(": out of memory\n", stderr);
-                break;
-            default:
-                fputc('\n', stderr);
-        }
-        return -1;
-    } else
-        return fd;
-}
-
 int open_server_socket(char service[]) {
     struct addrinfo *ai, *aip;
     int fd;
@@ -79,25 +106,46 @@ int open_server_socket(char service[]) {
 
     aip = ai;
     while (aip != NULL) {
-        fd = create_server_socket(aip);
-        aip = aip->ai_next;
+        fd = socket(aip->ai_family, aip->ai_socktype, aip->ai_protocol);
 
-        if (fd < 0)
+        if (fd < 0) {
+            error(E_SSOCKET, errno);
+            aip = aip->ai_next;
             continue;
+        }
+
+        if (bind(fd, aip->ai_addr, aip->ai_addrlen) != 0) {
+            error(E_SS_BIND, errno);
+            aip = aip->ai_next;
+            continue;
+        }
+
+        if (listen(fd, BACKLOG_SIZE) != 0) {
+            error(E_SS_LISTEN, errno);
+            aip = aip->ai_next;
+            continue;
+        }
 
         if (ai != NULL)
             freeaddrinfo(ai);
         return fd;
     }
+
+    if (ai != NULL)
+        freeaddrinfo(ai);
     return -1;
 }
 
 int main(int argc, char** argv) {
-    int ssocket;
+    int ssocket, socket;
 
     ssocket = open_server_socket((argc > 1) ? argv[1] : DEFAULT_SERVICE);
-    printf("ssocket=%d\n", ssocket);
+    puts("server is ready");
 
+    socket = accept(ssocket, NULL, NULL);
+    printf("%d\n", socket);
+
+    close(socket);
     close(ssocket);
     return 0;
 }
